b1b6e9db77ba0b16adcf54d1dc3827391befaa5f
Ruttaphong Worachartudomphong <maggotgluon@gmail.com>
2016-05-20 08:10:10 +0700

    blog

--- _site/js/tipuesearch/tipuesearch_content.js
+++ _site/js/tipuesearch/tipuesearch_content.js
@@ -2,63 +2,63 @@ var tipuesearch = {"pages": [
     {"title": "Home",
     "text": "Home.",
     "tags": "home, index",
-    "url": "http://maggotgluon.github.io/"},
+    "url": "http://ruttaphong.github.io/"},
     {"title": "About Me",
     "text": "A little about me, including my interests and some random facts!",
     "tags": "about, interests, random facts",
-    "url": "http://maggotgluon.github.io/#/about"},
+    "url": "http://ruttaphong.github.io/#/about"},
     {"title": "Resume",
     "text": "An overview of my resume, including my education, work history, development skills, and more.",
     "tags": "resume, skills, education, school, work, experience, awards, honors, languages, frameworks, databases",
-    "url": "http://maggotgluon.github.io/#/resume"},
+    "url": "http://ruttaphong.github.io/#/resume"},
     {"title": "Blog",
     "text": "My latest thoughts, posts, and inspirations.",
     "tags": "blog, latest posts, tutorials",
-    "url": "http://maggotgluon.github.io/blog"},
+    "url": "http://ruttaphong.github.io/blog"},
     {"title": "Portfolio",
     "text": "My portfolio of projects that I can show.",
     "tags": "portfolio, projects",
-    "url": "http://maggotgluon.github.io/#/portfolio"},
+    "url": "http://ruttaphong.github.io/#/portfolio"},
     {"title": "Contact",
     "text": "Feel free to email me or fill out the contact form to get in touch.",
     "tags": "contact, form, email",
-    "url": "http://maggotgluon.github.io/#/contact"},
+    "url": "http://ruttaphong.github.io/#/contact"},
 
     
     
     {"title": "Buddha Racha",
     "text": "Environment Projection and Video Visual for “ Buddha Racha “ @ New year’s eve pray at Royal Plaza, Sanam Luang · 2012 ",
     "tags": "Graphic Design, project, Apple Motion, Apple Final Cut Pro X, Adobe After Effect, and Pro Video Player",
-    "url": "http://maggotgluon.github.io/portfolio/buddha-racha"},
+    "url": "http://ruttaphong.github.io/portfolio/buddha-racha"},
     
     
     {"title": "มหาชาติมหาบุรุษ",
     "text": "Environment Projection and Video Visual for “ Maha Chart Maha Burut “ @ New year’s eve pray at Royal Plaza, Sanam Luang · 2013 ",
     "tags": "Graphic Design, project, Apple Motion, Apple Final Cut Pro X, Adobe After Effect, and Pro Video Player",
-    "url": "http://maggotgluon.github.io/portfolio/"},
+    "url": "http://ruttaphong.github.io/portfolio/"},
     
     
     {"title": "Rocking Rama",
     "text": "Motion Graphic and Multimedia for “Rocking Rama” Muisical; @ Vic Hua Hin · 2014 ",
     "tags": "Graphic Design, project, Apple Motion, Apple Final Cut Pro X, Adobe After Effect, and Pro Video Player",
-    "url": "http://maggotgluon.github.io/portfolio/rocking-rama"},
+    "url": "http://ruttaphong.github.io/portfolio/rocking-rama"},
     
     
     {"title": "Vivaha Pra Samut",
     "text": "Motion Graphic and Multimedia for “Vivaha Pra Samut” Muisical; @ Vic Hua Hin · 2012 ",
     "tags": "Graphic Design, project, Apple Motion, Apple Final Cut Pro X, Adobe After Effect, and Pro Video Player",
-    "url": "http://maggotgluon.github.io/portfolio/vivaha-pra-samut"},
+    "url": "http://ruttaphong.github.io/portfolio/vivaha-pra-samut"},
     
 
     
     {"title": "ทดสอบ",
     "text": "Heading One    This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap.  Heading Two    This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap.  Heading Three    This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap.  Heading Four    This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap.  Heading Five    This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap.  Heading Six    This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap.       This is italic text, made with em. This is italic text as well, made with i.     This is bold text, made with strong. This is bold text as well, made with b.     This is underlined text, made with u. This is inserted text, made with ins.     This is deleted (strikethrough) text, made with strike. This is also strikethrough text, but made with s. This is deleted text, made with del.     This is computer code, made with code. This is teletype text, made with tt.     This is text the user is supposed to enter in, or keyboard, made with kbd. This is a variable, made with var. This is sample program output, made with samp.     This is small text, made with small. This is big text, made with big.     This is a citation, made with cite. This is a short quotation in the middle of a sentence, made with q.     This sentence ends with a superscript. This sentence ends with a subscript.     This is an abbreviation, made with abbr: WWW. This is an acronym, made with acronym: NATO. This is a term that needs a definition, made with dfn: Yog-Sothoth.     This is an unordered list:     Koyaanisqatsi   Powaqqatsi   Naqoyqatsi     This is an ordered list:     Larry   Moe   Curly     This is a definition list:     Defintion Term   Definition data defining the previous term   Another Definition Term   Definition data defining the previous term     This is an address with breaks:     John Q. Public   123 Main Street   Anywhere, ST 12345     This is a long quotation inside a blockquote:          “I love songs about horses, railroads, land, judgement day, family, hard times, whiskey, courtship, marriage, adultery, separation, murder, war, prison, rambling, damnation, home, salvation, death, pride, humor, piety, rebellion, patriotism, larceny, determination, tragedy, rowdiness, heartbreak, and love. And Mother. And God.” ~ Johnny Cash        This is pre-formatted:   Start in column one.          Then indent to column ten.     Then back five spaces.     Here's a table with a border, a caption, colgroups, cols, thead, tfoot, &amp; tbody:     This is a table caption, made with caption                                  Table Header 1       Table Header 2       Table Header 3                       Table Footer 1       Table Footer 2       Table Footer 3                       TD 1.1       TD 1.2       TD 1.3                 TD 2.1       TD 2.2       TD 2.3                 TD 3.1       TD 3.2       TD 3.3          ",
     "tags": "general, ",
-    "url": "http://maggotgluon.github.io"},
+    "url": "http://ruttaphong.github.io"},
     
     {"title": "ทดสอบ 2",
     "text": "Heading One    This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap.  Heading Two    This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap.  Heading Three    This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap.  Heading Four    This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap.  Heading Five    This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap.  Heading Six    This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap. If not, we can repeat it over and over again until it does wrap. This is a paragraph of text that should be long enough to wrap.       This is italic text, made with em. This is italic text as well, made with i.     This is bold text, made with strong. This is bold text as well, made with b.     This is underlined text, made with u. This is inserted text, made with ins.     This is deleted (strikethrough) text, made with strike. This is also strikethrough text, but made with s. This is deleted text, made with del.     This is computer code, made with code. This is teletype text, made with tt.     This is text the user is supposed to enter in, or keyboard, made with kbd. This is a variable, made with var. This is sample program output, made with samp.     This is small text, made with small. This is big text, made with big.     This is a citation, made with cite. This is a short quotation in the middle of a sentence, made with q.     This sentence ends with a superscript. This sentence ends with a subscript.     This is an abbreviation, made with abbr: WWW. This is an acronym, made with acronym: NATO. This is a term that needs a definition, made with dfn: Yog-Sothoth.     This is an unordered list:     Koyaanisqatsi   Powaqqatsi   Naqoyqatsi     This is an ordered list:     Larry   Moe   Curly     This is a definition list:     Defintion Term   Definition data defining the previous term   Another Definition Term   Definition data defining the previous term     This is an address with breaks:     John Q. Public   123 Main Street   Anywhere, ST 12345     This is a long quotation inside a blockquote:          “I love songs about horses, railroads, land, judgement day, family, hard times, whiskey, courtship, marriage, adultery, separation, murder, war, prison, rambling, damnation, home, salvation, death, pride, humor, piety, rebellion, patriotism, larceny, determination, tragedy, rowdiness, heartbreak, and love. And Mother. And God.” ~ Johnny Cash        This is pre-formatted:   Start in column one.          Then indent to column ten.     Then back five spaces.     Here's a table with a border, a caption, colgroups, cols, thead, tfoot, &amp; tbody:     This is a table caption, made with caption                                  Table Header 1       Table Header 2       Table Header 3                       Table Footer 1       Table Footer 2       Table Footer 3                       TD 1.1       TD 1.2       TD 1.3                 TD 2.1       TD 2.2       TD 2.3                 TD 3.1       TD 3.2       TD 3.3          ",
     "tags": "general, ",
-    "url": "http://maggotgluon.github.io"}
+    "url": "http://ruttaphong.github.io"}
     
 ]};
