8efa8e2c4235fcf8751cb19b86ac1b3de0809497
Ruttaphong Worachartudomphong <maggotgluon@gmail.com>
2016-06-07 07:41:56 +0700

    static commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# [ruttaphong.github.io](http://ruttaphong.github.io)
-
-This is the source code for my personal site, [ruttaphong.github.io](http://ruttaphong.github.io/).
--- js/tipuesearch/tipuesearch_content.js
+++ js/tipuesearch/tipuesearch_content.js
@@ -1,44 +1,64 @@
----
----
-
 var tipuesearch = {"pages": [
     {"title": "Home",
     "text": "Home.",
     "tags": "home, index",
-    "url": "{{ site.url }}/"},
+    "url": "http://maggotgluon.github.io/"},
     {"title": "About Me",
     "text": "A little about me, including my interests and some random facts!",
     "tags": "about, interests, random facts",
-    "url": "{{ site.url }}/#/about"},
+    "url": "http://maggotgluon.github.io/#/about"},
     {"title": "Resume",
     "text": "An overview of my resume, including my education, work history, development skills, and more.",
     "tags": "resume, skills, education, school, work, experience, awards, honors, languages, frameworks, databases",
-    "url": "{{ site.url }}/#/resume"},
+    "url": "http://maggotgluon.github.io/#/resume"},
     {"title": "Blog",
     "text": "My latest thoughts, posts, and inspirations.",
     "tags": "blog, latest posts, tutorials",
-    "url": "{{ site.url }}/blog"},
+    "url": "http://maggotgluon.github.io/blog"},
     {"title": "Portfolio",
     "text": "My portfolio of projects that I can show.",
     "tags": "portfolio, projects",
-    "url": "{{ site.url }}/#/portfolio"},
+    "url": "http://maggotgluon.github.io/#/portfolio"},
     {"title": "Contact",
     "text": "Feel free to email me or fill out the contact form to get in touch.",
     "tags": "contact, form, email",
-    "url": "{{ site.url }}/#/contact"},
+    "url": "http://maggotgluon.github.io/#/contact"},
 
-    {% for project_page in site.data.projects %}
-    {% assign project = project_page[1] %}
-    {"title": "{{ project.title | xml_escape }}",
-    "text": "{{ project.description | truncatewords: 50 | markdownify | strip_html | replace_regex:'\n', ' ' }}",
-    "tags": "{{ project.category }}, project, {{ project.technology | array_to_sentence_string }}",
-    "url": "{{ project.dir | prepend: '/' | prepend: site.url }}"},
-    {% endfor %}
+    
+    
+    {"title": "Buddha Racha",
+    "text": "Environment Projection and Video Visual for “ Buddha Racha “ @ New year’s eve pray at Royal Plaza, Sanam Luang · 2012 ",
+    "tags": "Graphic Design, project, Apple Motion, Apple Final Cut Pro X, Adobe After Effect, and Pro Video Player",
+    "url": "http://maggotgluon.github.io/portfolio/buddha-racha"},
+    
+    
+    {"title": "มหาชาติมหาบุรุษ",
+    "text": "Environment Projection and Video Visual for “ Maha Chart Maha Burut “ @ New year’s eve pray at Royal Plaza, Sanam Luang · 2013 ",
+    "tags": "Graphic Design, project, Apple Motion, Apple Final Cut Pro X, Adobe After Effect, and Pro Video Player",
+    "url": "http://maggotgluon.github.io/portfolio/"},
+    
+    
+    {"title": "Rocking Rama",
+    "text": "Motion Graphic and Multimedia for “Rocking Rama” Muisical; @ Vic Hua Hin · 2014 ",
+    "tags": "Graphic Design, project, Apple Motion, Apple Final Cut Pro X, Adobe After Effect, and Pro Video Player",
+    "url": "http://maggotgluon.github.io/portfolio/rocking-rama"},
+    
+    
+    {"title": "Vivaha Pra Samut",
+    "text": "Motion Graphic and Multimedia for “Vivaha Pra Samut” Muisical; @ Vic Hua Hin · 2012 ",
+    "tags": "Graphic Design, project, Apple Motion, Apple Final Cut Pro X, Adobe After Effect, and Pro Video Player",
+    "url": "http://maggotgluon.github.io/portfolio/vivaha-pra-samut"},
+    
 
-    {% for post in site.posts %}
-    {"title": "{{ post.title | xml_escape }}",
-    "text": "{{ post.excerpt | markdownify | strip_html | replace_regex:'\n', ' ' }}",
-    "tags": "{{ post.category }}, {{ post.tags | array_to_sentence_string }}",
-    "url": "{{ post.dir | prepend: site.url }}"}{% unless forloop.last %},{% endunless %}
-    {% endfor %}
+    
+    {"title": "JavaScript Parallax",
+    "text": " ",
+    "tags": "Java, ",
+    "url": "http://maggotgluon.github.io"},
+    
+    {"title": "JavaScriptเปลี่ยนพื้นหลังตามช่วงเวลาของวัน",
+    "text": "วันนี้ขอ มาแบ่งปันความรู้เกี่ยวกับ Java Script กันสักหน่อยครับ ผมเองได้มีโอกาสทำ Project Website หนึ่งที่ลูกค้าอยากให้ ภาพสื่อถึงบรรยากาศ ผมเลยเสนอว่า ถ้า ผู้ใช้งาน เข้าเว็บตอนเช้า ก็ให้พื้นหลังเป็นยามเช้า เข้าตอนเย็นเห็นพระอาทิตย์ตก เข้ามาเว็บตอนกลางคืน ก็เห็นพระจันทร์ ประมาณนี้ ลูกค้าก็เห็นดีด้วย ผมเลยได้โอกาสเขียน Snippet Java เล็กๆ ขึ้นเพื่องานนี้  ",
+    "tags": "Java, ",
+    "url": "http://maggotgluon.github.io"}
+    
 ]};
