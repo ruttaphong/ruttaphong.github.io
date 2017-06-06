ae6e6ae065ca95154dd56c1b875a425b8cc76b56
Ruttaphong Worachartudomphong <maggotgluon@gmail.com>
2017-06-06 14:55:10 +0700

    Update

---
+++ _posts/2017-06-05-hello-world.md
@@ -0,0 +1,11 @@
+---
+title: Obligatory "Hello World"
+layout: post
+date: '2017-06-05 07:00:00'
+category: general
+type: hentry
+---
+
+Just like how every programmer first starts with a "Hello World" program, it's
+basically mandatory for "Hello World" to be every programmer's first blog post!
+ So, here it is: **Hello World!**
---
+++ _site/js/tipuesearch/tipuesearch_content.js
@@ -0,0 +1,40 @@
+var tipuesearch = {"pages": [
+    {"title": "Home",
+    "text": "Home.",
+    "tags": "home, index",
+    "url": "http://localhost:4000/"},
+    {"title": "About Me",
+    "text": "A little about me, including my interests and some random facts!",
+    "tags": "about, interests, random facts",
+    "url": "http://localhost:4000/#/about"},
+    {"title": "Resume",
+    "text": "An overview of my resume, including my education, work history, development skills, and more.",
+    "tags": "resume, skills, education, school, work, experience, awards, honors, languages, frameworks, databases",
+    "url": "http://localhost:4000/#/resume"},
+    {"title": "Blog",
+    "text": "My latest thoughts, posts, and inspirations.",
+    "tags": "blog, latest posts, tutorials",
+    "url": "http://localhost:4000/blog"},
+    {"title": "Portfolio",
+    "text": "My portfolio of projects that I can show.",
+    "tags": "portfolio, projects",
+    "url": "http://localhost:4000/#/portfolio"},
+    {"title": "Contact",
+    "text": "Feel free to email me or fill out the contact form to get in touch.",
+    "tags": "contact, form, email",
+    "url": "http://localhost:4000/#/contact"},
+
+    
+
+    
+    {"title": "Obligatory &quot;Hello World&quot;",
+    "text": "Just like how every programmer first starts with a “Hello World” program, it’s basically mandatory for “Hello World” to be every programmer’s first blog post!  So, here it is: Hello World! ",
+    "tags": "general, ",
+    "url": "http://localhost:4000"},
+    
+    {"title": "JavaScriptเปลี่ยนพื้นหลังตามช่วงเวลาของวัน",
+    "text": "วันนี้ขอ มาแบ่งปันความรู้เกี่ยวกับ Java Script กันสักหน่อยครับ ผมเองได้มีโอกาสทำ Project Website หนึ่งที่ลูกค้าอยากให้ ภาพสื่อถึงบรรยากาศ ผมเลยเสนอว่า ถ้า ผู้ใช้งาน เข้าเว็บตอนเช้า ก็ให้พื้นหลังเป็นยามเช้า เข้าตอนเย็นเห็นพระอาทิตย์ตก เข้ามาเว็บตอนกลางคืน ก็เห็นพระจันทร์ ประมาณนี้ ลูกค้าก็เห็นดีด้วย ผมเลยได้โอกาสเขียน Snippet Java เล็กๆ ขึ้นเพื่องานนี้  ",
+    "tags": "tutorial, javascript, tutorial, and jQuery",
+    "url": "http://localhost:4000"}
+    
+]};
